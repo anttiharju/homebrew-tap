bce8ebfaa9fa84caa0614cbbdad8d8c4340c1aff
Antti Harju <antti@harju.io>
2025-01-12 20:52:30 +0200

    Fix homepage

--- scripts/render.py
+++ scripts/render.py
@@ -5,7 +5,7 @@ from helpers import FormulaConfig, render_formula
 config = FormulaConfig(
     app_name="vmatch-golangci-lint",
     description="Wrapper that automatically calls the golangci-lint version matching your project",
-    homepage="https://anttiharju.dev/vmatch/",
+    homepage="https://anttiharju.dev/vmatch",
     url="https://github.com/anttiharju/vmatch/archive/refs/tags/build5.tar.gz",
     go_version="1.23",
 )
