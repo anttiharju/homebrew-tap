e40f6bfa3bd39c5c077e4c6f39ff75fb09a0c096
anttiharju <anttiharju@users.noreply.github.com>
2025-01-12 20:35:40 +0000

    Update vmatch-golangci-lint to build6

--- Formula/vmatch-golangci-lint.rb
+++ Formula/vmatch-golangci-lint.rb
@@ -1,5 +1,5 @@
 class VmatchGolangciLint < Formula
-    desc "Wrapper that automatically calls the golangci-lint version matching your project"
+    desc "something different to trigger commit"
     homepage "https://anttiharju.dev/vmatch"
 
     url "https://github.com/anttiharju/vmatch/archive/refs/tags/build6.tar.gz"
