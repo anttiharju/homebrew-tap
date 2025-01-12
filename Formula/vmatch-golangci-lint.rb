ee35e9efe228c30e9bed9838c80dd4f8c2697366
Antti Harju <antti@harju.io>
2025-01-12 20:53:39 +0200

    Render with new params

--- Formula/vmatch-golangci-lint.rb
+++ Formula/vmatch-golangci-lint.rb
@@ -1,9 +1,9 @@
 class VmatchGolangciLint < Formula
     desc "Wrapper that automatically calls the golangci-lint version matching your project"
-    homepage "https://anttiharju.dev/vmatch/"
+    homepage "https://anttiharju.dev/vmatch"
 
-    url "https://github.com/anttiharju/vmatch/archive/refs/tags/build5.tar.gz"
-    sha256 "33d87b5789ecd5920e746ce0e8762ef09f8be4d746c6d319801c74f593d7f6ce"
+    url "https://github.com/anttiharju/vmatch/archive/refs/tags/build6.tar.gz"
+    sha256 "3381b2e0cfc73d6f6c76f0114707178519d283ffe47220e679ae83bf6633c7db"
     head "https://github.com/anttiharju/vmatch"
 
     depends_on "go@1.23" => :build
@@ -14,7 +14,7 @@ class VmatchGolangciLint < Formula
         bin_path = buildpath/"src/github.com/anttiharju/vmatch"
         bin_path.install Dir["*"]
         cd bin_path do
-          system "go", "build", "-C", "cmd/vmatch-golangci-lint", "-ldflags", "-s -w -buildid=brew-build5 -X github.com/anttiharju/vmatch/pkg/exit.appName=vmatch-golangci-lint"
+          system "go", "build", "-C", "cmd/vmatch-golangci-lint", "-ldflags", "-s -w -buildid=brew-build6"
           bin.install "cmd/vmatch-golangci-lint/vmatch-golangci-lint"
         end
     end
