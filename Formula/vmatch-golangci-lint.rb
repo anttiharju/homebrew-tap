c5ef29011b0d27d5009c233db67e5b014a4de48e
Antti Harju <antti@harju.io>
2025-01-12 14:13:55 +0200

    Convert indentation to spaces

--- Formula/vmatch-golangci-lint.rb
+++ Formula/vmatch-golangci-lint.rb
@@ -1,25 +1,25 @@
 class VmatchGolangciLint < Formula
-	desc "Wrapper that automatically calls the golangci-lint version matching your project"
-	homepage "https://anttiharju.dev/vmatch/"
+    desc "Wrapper that automatically calls the golangci-lint version matching your project"
+    homepage "https://anttiharju.dev/vmatch/"
 
-	url "https://github.com/anttiharju/vmatch/archive/refs/tags/build5.tar.gz"
-	sha256 "33d87b5789ecd5920e746ce0e8762ef09f8be4d746c6d319801c74f593d7f6ce"
-	head "https://github.com/anttiharju/vmatch"
+    url "https://github.com/anttiharju/vmatch/archive/refs/tags/build5.tar.gz"
+    sha256 "33d87b5789ecd5920e746ce0e8762ef09f8be4d746c6d319801c74f593d7f6ce"
+    head "https://github.com/anttiharju/vmatch"
 
-	depends_on "go@1.23" => :build
+    depends_on "go@1.23" => :build
 
-	def install
-		ENV["GOPATH"] = buildpath
+    def install
+        ENV["GOPATH"] = buildpath
 
-		bin_path = buildpath/"src/github.com/anttiharju/vmatch"
-		bin_path.install Dir["*"]
-		cd bin_path do
-		  system "go", "build", "-C", "cmd/vmatch-golangci-lint", "-ldflags", "-s -w -buildid=brew-build5 -X github.com/anttiharju/vmatch/pkg/exit.appName=vmatch-golangci-lint"
-		  bin.install "cmd/vmatch-golangci-lint/vmatch-golangci-lint"
-		end
-	  end
+        bin_path = buildpath/"src/github.com/anttiharju/vmatch"
+        bin_path.install Dir["*"]
+        cd bin_path do
+          system "go", "build", "-C", "cmd/vmatch-golangci-lint", "-ldflags", "-s -w -buildid=brew-build5 -X github.com/anttiharju/vmatch/pkg/exit.appName=vmatch-golangci-lint"
+          bin.install "cmd/vmatch-golangci-lint/vmatch-golangci-lint"
+        end
+      end
 
-	test do
-		system "true"
-	end
+    test do
+        system "true"
+    end
 end
