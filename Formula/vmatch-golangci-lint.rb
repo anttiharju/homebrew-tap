006e7083ee55912eebf4cd2538b598507c0d617d
anttiharju <anttiharju@users.noreply.github.com>
2025-01-13 20:32:02 +0000

    Update vMatch formulas to build17
    https://github.com/anttiharju/vmatch/blob/main/.github/workflows/homebrew_tap.yml

--- Formula/vmatch-golangci-lint.rb
+++ Formula/vmatch-golangci-lint.rb
@@ -2,8 +2,8 @@ class VmatchGolangciLint < Formula
     desc "Wrapper that automatically calls the golangci-lint version matching your project"
     homepage "https://anttiharju.dev/vmatch"
 
-    url "https://api.github.com/repos/anttiharju/vmatch/tarball/build16"
-    sha256 "b92a4e1ed855fe89f4b86a57d954643053f4d4eda2bd612af4cde6dc793fbe37"
+    url "https://api.github.com/repos/anttiharju/vmatch/tarball/build17"
+    sha256 "252e75876a7fdb729e758174f56136100d541735ca60ac738e2c938a408cefab"
     head "https://github.com/anttiharju/vmatch"
 
     depends_on "go@1.23" => :build
@@ -14,7 +14,7 @@ class VmatchGolangciLint < Formula
         bin_path = buildpath/"src/github.com/anttiharju/vmatch"
         bin_path.install Dir["*"]
         cd bin_path do
-          system "go", "build", "-C", "cmd/vmatch-golangci-lint", "-ldflags", "-s -w -buildid=brew-build16"
+          system "go", "build", "-C", "cmd/vmatch-golangci-lint", "-ldflags", "-s -w -buildid=brew-build17"
           bin.install "cmd/vmatch-golangci-lint/vmatch-golangci-lint"
         end
     end
