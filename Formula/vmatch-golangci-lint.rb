658f8e76b7e49483255a3c1cf508f85435fa7b3d
anttiharju <anttiharju@users.noreply.github.com>
2025-01-13 20:26:50 +0000

    Update vMatch formulas to build16

--- Formula/vmatch-golangci-lint.rb
+++ Formula/vmatch-golangci-lint.rb
@@ -2,8 +2,8 @@ class VmatchGolangciLint < Formula
     desc "Wrapper that automatically calls the golangci-lint version matching your project"
     homepage "https://anttiharju.dev/vmatch"
 
-    url "https://api.github.com/repos/anttiharju/vmatch/tarball/build10"
-    sha256 "0d79c41b6de94919536e1bf5a2d4ec0c9933f55f0e1870cccd991749c03dba1d"
+    url "https://api.github.com/repos/anttiharju/vmatch/tarball/build16"
+    sha256 "b92a4e1ed855fe89f4b86a57d954643053f4d4eda2bd612af4cde6dc793fbe37"
     head "https://github.com/anttiharju/vmatch"
 
     depends_on "go@1.23" => :build
@@ -14,7 +14,7 @@ class VmatchGolangciLint < Formula
         bin_path = buildpath/"src/github.com/anttiharju/vmatch"
         bin_path.install Dir["*"]
         cd bin_path do
-          system "go", "build", "-C", "cmd/vmatch-golangci-lint", "-ldflags", "-s -w -buildid=brew-build10"
+          system "go", "build", "-C", "cmd/vmatch-golangci-lint", "-ldflags", "-s -w -buildid=brew-build16"
           bin.install "cmd/vmatch-golangci-lint/vmatch-golangci-lint"
         end
     end
