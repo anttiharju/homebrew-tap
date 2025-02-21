2db6bf390aa497569f69f430bf0893d22a40d390
anttiharju <anttiharju@users.noreply.github.com>
2025-02-21 20:34:57 +0000

    Update vmatch formula to build45
    https://github.com/anttiharju/vmatch/blob/eb015ae51102b1d5b16422069561814506017753/.github/workflows/distribute.yml

--- Formula/vmatch.rb
+++ Formula/vmatch.rb
@@ -2,8 +2,8 @@ class Vmatch < Formula
     desc "Wrapper that automatically calls the golangci-lint version matching your project"
     homepage "https://anttiharju.dev/vmatch"
 
-    url "https://api.github.com/repos/anttiharju/vmatch/tarball/build44"
-    sha256 "d039b3a65036b62b8c74bb0f39d4a3b37a0d7b6c7fdcb746bad6c33ff3f91874"
+    url "https://api.github.com/repos/anttiharju/vmatch/tarball/build45"
+    sha256 "3b77d03de98aa07ef0fef5ec16259e70c94d39ba6cc360fe00d68b36bf8d2462"
     head "https://github.com/anttiharju/vmatch"
 
     depends_on "go@1.23" => :build
@@ -14,7 +14,7 @@ class Vmatch < Formula
         bin_path = buildpath/"src/github.com/anttiharju/vmatch"
         bin_path.install Dir["*"]
         cd bin_path do
-          system "go", "build", "-ldflags", "-s -w -buildid=brew-build44"
+          system "go", "build", "-ldflags", "-s -w -buildid=brew-build45"
           bin.install "vmatch"
         end
     end
