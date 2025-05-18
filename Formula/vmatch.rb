f41332760f1c0113012fb5ee6bacd4a3cb674bcb
anttiharju <anttiharju@users.noreply.github.com>
2025-05-18 20:38:01 +0000

    Update vmatch formula to build105
    https://github.com/anttiharju/vmatch/blob/cdf2cfc40b93b58e6799975e7cbcf90c026c1f49/.github/workflows/distribute.yml

--- Formula/vmatch.rb
+++ Formula/vmatch.rb
@@ -1,26 +1,27 @@
 # frozen_string_literal: true
 
+# testing
 # Homebrew formula for vmatch - a tool that automates
 # matching golangci-lint and Go versions to your project.
 #
 # This formula installs the vmatch binary and its dependencies.
 class Vmatch < Formula
-  desc 'golangci-lint + Go version automation'
+  desc 'Go & golangci-lint version automation'
   homepage 'https://anttiharju.dev/vmatch'
 
-  url 'https://api.github.com/repos/anttiharju/vmatch/tarball/build104'
-  sha256 '20a42dd7f1fd1ad4c04e6839abaea9441dbc13c25063f8e0889ef44d4af83c41'
+  url 'https://api.github.com/repos/anttiharju/vmatch/tarball/build105'
+  sha256 '99886aaa4357105077c996bdd41c8d29c31d8d8bb30e09478991bc950c71e420'
   head 'https://github.com/anttiharju/vmatch'
   license 'GPL-3.0-only'
 
   depends_on 'go@1.23' => :build
 
   bottle do
-    root_url 'https://github.com/anttiharju/vmatch/releases/download/build104'
+    root_url 'https://github.com/anttiharju/vmatch/releases/download/build105'
     rebuild 1
-    sha256 cellar: :any, arm64_sequoia: '2ec23352bb57d817d838a491f5fd603fe0ccfe71816fb6c98b2362826f9d63f1'
-    sha256 cellar: :any, x86_64_linux: 'b846e2acbb877582a0bfeb025c46779cfbf241fff82dc470fc89ccac7dba34ed'
-    sha256 cellar: :any, arm64_sonoma: '014ed21a062c61750c03897b8c795d334fe068183c733cad9436685ca0d07a26'
+    sha256 cellar: :any, arm64_sequoia: 'f2c7d2beac320dbefcdf87dcc6c2b9a01254e7f636352d5271c8dc33d49ea95a'
+    sha256 cellar: :any, x86_64_linux: '757a08cc0882dd6552621339a8422267c82e1f5f7f5d53d7b42fecff0d4cf7da'
+    sha256 cellar: :any, arm64_sonoma: '5b0beeb3d67eb87ca2beb80f205b49399275d67395b1aaadbdd02571c5820cc6'
   end
 
   def install
@@ -29,7 +30,7 @@ class Vmatch < Formula
     bin_path = buildpath / 'src/github.com/anttiharju/vmatch'
     bin_path.install Dir['*']
     cd bin_path do
-      system 'go', 'build', '-ldflags', '-s -w -buildid=brew-build104'
+      system 'go', 'build', '-ldflags', '-s -w -buildid=brew-build105'
       bin.install 'vmatch'
     end
   end
