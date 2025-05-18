c699b27e9064d1314a4e394c1356819bd70de3f5
anttiharju <anttiharju@users.noreply.github.com>
2025-05-18 15:23:06 +0000

    Update vmatch formula to build103
    https://github.com/anttiharju/vmatch/blob/56328747142f7a0c5769b4b8f8de01a356d7b989/.github/workflows/distribute.yml

--- Formula/vmatch.rb
+++ Formula/vmatch.rb
@@ -8,12 +8,20 @@ class Vmatch < Formula
   desc 'golangci-lint + Go version automation'
   homepage 'https://anttiharju.dev/vmatch'
 
-  url 'https://api.github.com/repos/anttiharju/vmatch/tarball/build102'
-  sha256 '74936e1f5b1695c071f6953ea5399aacb4238c3c08ea49b0fda16cf5443d5eb0'
+  url 'https://api.github.com/repos/anttiharju/vmatch/tarball/build103'
+  sha256 '1bf9103edbb8f5b79c4d616714e6fd3d0698cc95dc660c338e1bd47e5b4191e6'
   head 'https://github.com/anttiharju/vmatch'
-  license "GPL-3.0-only"
+  license 'GPL-3.0-only'
 
   depends_on 'go@1.23' => :build
+
+  bottle do
+    root_url 'https://github.com/anttiharju/vmatch/releases/download/build103'
+    rebuild 1
+    sha256 cellar: :any, x86_64_linux: '8b32aa7df8aeac78049a1de1bdda32d4f220d46d10ed2f44939ce01685252d68'
+    sha256 cellar: :any, arm64_sequoia: '1906a09b2438ecc84ba39adc1f5146ce74b95f4462743cfffe808bbd915f2351'
+    sha256 cellar: :any, arm64_sonoma: 'd389c8bd92d195d1740f168f9e7140035d0521b89c083b022ef59d2a74f9c9ed'
+  end
 
   def install
     ENV['GOPATH'] = buildpath
@@ -21,7 +29,7 @@ class Vmatch < Formula
     bin_path = buildpath / 'src/github.com/anttiharju/vmatch'
     bin_path.install Dir['*']
     cd bin_path do
-      system 'go', 'build', '-ldflags', '-s -w -buildid=brew-build102'
+      system 'go', 'build', '-ldflags', '-s -w -buildid=brew-build103'
       bin.install 'vmatch'
     end
   end
