f27d6cbf34e523266caafc4a4a4f417d844397fe
anttiharju <anttiharju@users.noreply.github.com>
2025-05-21 19:07:29 +0000

    Update vmatch formula to build109
    https://github.com/anttiharju/vmatch/blob/f438c7dfbac7ecf8e43db68d86153f556f7985d9/.github/workflows/distribute.yml

--- Formula/vmatch.rb
+++ Formula/vmatch.rb
@@ -8,19 +8,19 @@ class Vmatch < Formula
   desc 'Go & golangci-lint version automation'
   homepage 'https://anttiharju.dev/vmatch'
 
-  url 'https://api.github.com/repos/anttiharju/vmatch/tarball/build108'
-  sha256 '75587490e3743c0924d077f1786f7bc0ff2b26f261451b9ce9f4b4beb65223b0'
+  url 'https://api.github.com/repos/anttiharju/vmatch/tarball/build109'
+  sha256 '6d98b32c8ad352368d151129ddf1d85f4a8987f63a1222f3979b0b8f736178de'
   head 'https://github.com/anttiharju/vmatch'
   license 'GPL-3.0-only'
 
-  depends_on 'go@1.23' => :build
+  depends_on 'go@1.24' => :build
 
   bottle do
-    root_url 'https://github.com/anttiharju/vmatch/releases/download/build108'
+    root_url 'https://github.com/anttiharju/vmatch/releases/download/build109'
     rebuild 1
-    sha256 cellar: :any, arm64_sonoma: '02183aea09f17f700acc6e9224cd543f8561a03a74409c0aa6ef62b6e89a8015'
-    sha256 cellar: :any, arm64_sequoia: '1ef07e6d6c252d793445e30b2375560b8924e379b7f6bc0d6f57ae17c6d49808'
-    sha256 cellar: :any, x86_64_linux: '2ddadf8f42ed80456ef294f4587118433d62cb0858e09ff076651322bb3bc51f'
+    sha256 cellar: :any, arm64_sequoia: 'c8cae83a84ad341ae0d1b547ac994b46f4dd5e373359cd2e6ff8a7f7164a18e3'
+    sha256 cellar: :any, x86_64_linux: 'a4b76f5541dbac61e9063d2a7cbc20681b95a5d9eda266d4f2b806daa728cde2'
+    sha256 cellar: :any, arm64_sonoma: '128490b7bd01610a2c9789d07eefeaff80f5ece28225bc055b06ec226ea1ed05'
   end
 
   def install
@@ -29,7 +29,7 @@ class Vmatch < Formula
     bin_path = buildpath / 'src/github.com/anttiharju/vmatch'
     bin_path.install Dir['*']
     cd bin_path do
-      system 'go', 'build', '-ldflags', '-s -w -buildid=brew-build108'
+      system 'go', 'build', '-ldflags', '-s -w -buildid=brew-build109'
       bin.install 'vmatch'
     end
   end
