4880bea22f5b5522fbc97d62c57326cd29fc0924
anttiharju <anttiharju@users.noreply.github.com>
2025-05-25 18:46:18 +0000

    Update vmatch formula to v0.1.6
    https://github.com/anttiharju/vmatch/blob/83c9aa837cc86bbce57b3b7381666e5d5b523939/.github/workflows/distribute.yml

--- Formula/vmatch.rb
+++ Formula/vmatch.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# testing
 # Homebrew formula for vmatch - a tool that automates
 # matching golangci-lint and Go versions to your project.
 #
@@ -8,19 +9,19 @@ class Vmatch < Formula
   desc 'Go & golangci-lint version automation'
   homepage 'https://anttiharju.dev/vmatch'
 
-  url 'https://api.github.com/repos/anttiharju/vmatch/tarball/v0.1.5'
-  sha256 'b2e5196744ae26322ac002d12ad535b18e30e7843cd138c82f003b5db458fe0e'
+  url 'https://api.github.com/repos/anttiharju/vmatch/tarball/v0.1.6'
+  sha256 'f99201a9ec3bb1f8fd62ebb56ec93efdcf8ab5080fa2378bbf5fa2c3357b9686'
   head 'https://github.com/anttiharju/vmatch'
   license 'GPL-3.0-only'
 
   depends_on 'go@1.24' => :build
 
   bottle do
-    root_url 'https://github.com/anttiharju/vmatch/releases/download/v0.1.5'
+    root_url 'https://github.com/anttiharju/vmatch/releases/download/v0.1.6'
     rebuild 1
-    sha256 cellar: :any, arm64_sequoia: 'd2a645478c7e55eb2472d366c4474caf2b49201fd609abdbb2dffd7b20755b4f'
-    sha256 cellar: :any, arm64_sonoma: '3faeb09f3a4d55e0dab29a9cadd8204e46e48bf5693f4aac4055e8cbd087865c'
-    sha256 cellar: :any, x86_64_linux: 'ada63be0d68fe5bf1db914e2a43db902dfdc6df464fe633ca5817faea7547d73'
+    sha256 cellar: :any, arm64_sonoma: '7e18e3177e24ff0d288670f2a855ead405bfcefbc97a6ad95e0c2e1775462e50'
+    sha256 cellar: :any, x86_64_linux: '8e6c457eb6864ba860341f587d7e148f2a60e8c2195eacb0f126c89ca91f6365'
+    sha256 cellar: :any, arm64_sequoia: '9da12e04f2cd8dc3f6bfb00e27133c36b6290f95120bf40162b38c3ed928ae50'
   end
 
   def install
@@ -29,7 +30,7 @@ class Vmatch < Formula
     bin_path = buildpath / 'src/github.com/anttiharju/vmatch'
     bin_path.install Dir['*']
     cd bin_path do
-      system 'go', 'build', '-ldflags', '-s -w -buildid=brew-v0.1.5', '-trimpath'
+      system 'go', 'build', '-ldflags', '-s -w -buildid=brew-v0.1.6', '-trimpath'
       bin.install 'vmatch'
     end
   end
