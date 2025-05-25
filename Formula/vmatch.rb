d966f6a2692fe968ed6ab2f3b75cabeecb27f418
anttiharju <anttiharju@users.noreply.github.com>
2025-05-25 16:01:17 +0000

    Update vmatch formula to v0.1.2
    https://github.com/anttiharju/vmatch/blob/c3435db10c9a26f4458bef379a3bd397f6ae870e/.github/workflows/distribute.yml

--- Formula/vmatch.rb
+++ Formula/vmatch.rb
@@ -8,19 +8,19 @@ class Vmatch < Formula
   desc 'Go & golangci-lint version automation'
   homepage 'https://anttiharju.dev/vmatch'
 
-  url 'https://api.github.com/repos/anttiharju/vmatch/tarball/v0.1.1'
-  sha256 '9b5ff0e7fd26ccd4554c7812cd422f6cd7354cf5eca0af18846971616ada2a98'
+  url 'https://api.github.com/repos/anttiharju/vmatch/tarball/v0.1.2'
+  sha256 'f8950a007bb28ff12b4d56f509210ac516921056380b0da2d9f6a3fb87dc7d94'
   head 'https://github.com/anttiharju/vmatch'
   license 'GPL-3.0-only'
 
   depends_on 'go@1.24' => :build
 
   bottle do
-    root_url 'https://github.com/anttiharju/vmatch/releases/download/v0.1.1'
+    root_url 'https://github.com/anttiharju/vmatch/releases/download/v0.1.2'
     rebuild 1
-    sha256 cellar: :any, arm64_sonoma: 'e625af1cdc45e0a746127198f1a541a704272a9d0fb0e58e4bfee0fefbb4146f'
-    sha256 cellar: :any, x86_64_linux: 'ce4cea839465a4d885aef64071fe80b161d94af28c65ef4214165a1777a27bce'
-    sha256 cellar: :any, arm64_sequoia: '292bc3cf479c7d21b1ae24f915c7c51532a484d494e2b177da660a2e005daf4c'
+    sha256 cellar: :any, arm64_sequoia: '125f8560a4e551526dfec23fc19a94e8bf43d2f16a94af5a8b726a2d31414202'
+    sha256 cellar: :any, arm64_sonoma: '1059abbb24428161562342ae337dd36d4a15502e8b5ee44925461ecb57218559'
+    sha256 cellar: :any, x86_64_linux: '347ade356c0e24edc35b794aad0776ab5f052e3f23bdf79fa776793218038df4'
   end
 
   def install
@@ -29,7 +29,7 @@ class Vmatch < Formula
     bin_path = buildpath / 'src/github.com/anttiharju/vmatch'
     bin_path.install Dir['*']
     cd bin_path do
-      system 'go', 'build', '-ldflags', '-s -w -buildid=brew-v0.1.1', '-trimpath'
+      system 'go', 'build', '-ldflags', '-s -w -buildid=brew-v0.1.2', '-trimpath'
       bin.install 'vmatch'
     end
   end
