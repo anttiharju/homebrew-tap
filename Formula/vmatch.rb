8b734b0037e5aff3e349cccf607cfac2d1b7f96a
anttiharju <anttiharju@users.noreply.github.com>
2025-05-25 17:27:17 +0000

    Update vmatch formula to v0.1.3
    https://github.com/anttiharju/vmatch/blob/6e7865b410f2d6375cb7d0daf74ea9e7f69b7d88/.github/workflows/distribute.yml

--- Formula/vmatch.rb
+++ Formula/vmatch.rb
@@ -8,19 +8,19 @@ class Vmatch < Formula
   desc 'Go & golangci-lint version automation'
   homepage 'https://anttiharju.dev/vmatch'
 
-  url 'https://api.github.com/repos/anttiharju/vmatch/tarball/v0.1.2'
-  sha256 'f8950a007bb28ff12b4d56f509210ac516921056380b0da2d9f6a3fb87dc7d94'
+  url 'https://api.github.com/repos/anttiharju/vmatch/tarball/v0.1.3'
+  sha256 'a63689ceb4861aaff92ff47974a953cc4d02df2026d0162805b6148404ea79bc'
   head 'https://github.com/anttiharju/vmatch'
   license 'GPL-3.0-only'
 
   depends_on 'go@1.24' => :build
 
   bottle do
-    root_url 'https://github.com/anttiharju/vmatch/releases/download/v0.1.2'
+    root_url 'https://github.com/anttiharju/vmatch/releases/download/v0.1.3'
     rebuild 1
-    sha256 cellar: :any, arm64_sequoia: '125f8560a4e551526dfec23fc19a94e8bf43d2f16a94af5a8b726a2d31414202'
-    sha256 cellar: :any, arm64_sonoma: '1059abbb24428161562342ae337dd36d4a15502e8b5ee44925461ecb57218559'
-    sha256 cellar: :any, x86_64_linux: '347ade356c0e24edc35b794aad0776ab5f052e3f23bdf79fa776793218038df4'
+    sha256 cellar: :any, arm64_sequoia: '7ada9fe1fe31718006ff7bcb12fcb61011f453c8a5b4903cb4193e8664f28d1f'
+    sha256 cellar: :any, x86_64_linux: 'caca62b2d3852224b37ba82a5b0e715b744e8f63f42a2ca720f940dfb690eac3'
+    sha256 cellar: :any, arm64_sonoma: '2208c1f2b2165d170db300809a053048a5e60d0f4b9bc22e3a5798e74282019b'
   end
 
   def install
@@ -29,12 +29,12 @@ class Vmatch < Formula
     bin_path = buildpath / 'src/github.com/anttiharju/vmatch'
     bin_path.install Dir['*']
     cd bin_path do
-      system 'go', 'build', '-ldflags', '-s -w -buildid=brew-v0.1.2', '-trimpath'
+      system 'go', 'build', '-ldflags', '-s -w -buildid=brew-v0.1.3', '-trimpath'
       bin.install 'vmatch'
     end
   end
 
   test do
-    system 'true'
+    system 'vmatch', 'doctor'
   end
 end
