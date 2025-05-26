c0926e6307c6078f2c17af9edfb7e27776412b28
anttiharju <anttiharju@users.noreply.github.com>
2025-05-26 18:04:19 +0000

    Update vmatch formula to v0.1.8
    https://github.com/anttiharju/vmatch/blob/590267a641b36351414dafe2294c6256b151eff1/.github/workflows/distribute.yml

--- Formula/vmatch.rb
+++ Formula/vmatch.rb
@@ -1,41 +1,54 @@
 # frozen_string_literal: true
 
-# testing
-# Homebrew formula for vmatch - a tool that automates
-# matching golangci-lint and Go versions to your project.
-#
-# This formula installs the vmatch binary and its dependencies.
 class Vmatch < Formula
   desc 'Go & golangci-lint version automation'
   homepage 'https://anttiharju.dev/vmatch'
-
-  url 'https://api.github.com/repos/anttiharju/vmatch/tarball/v0.1.7'
-  sha256 'b32752aedb822682ebd5084f2da531e405512b09f533118364781ba2ef2eaa69'
-  head 'https://github.com/anttiharju/vmatch'
+  version '0.1.8'
   license 'GPL-3.0-only'
 
-  depends_on 'go@1.24' => :build
+  on_macos do
+    if Hardware::CPU.intel?
+      url 'https://github.com/anttiharju/vmatch/releases/download/v0.1.8/vmatch-darwin-amd64.tar.gz'
+      sha256 ''
 
-  bottle do
-    root_url 'https://github.com/anttiharju/vmatch/releases/download/v0.1.7'
-    rebuild 1
-    sha256 cellar: :any, arm64_sequoia: '3313f12905e51c9f5a1527f903e9b7ecc4e5d9e9ed4acff2ac76e3809ca6ae03'
-    sha256 cellar: :any, arm64_sonoma: 'e3d286b2bd2bf2d6d4a720bca7c5b233196f3a00e3f1791d06b59a4b6ac71190'
-    sha256 cellar: :any, x86_64_linux: '114bc6a09d5999bbef5f07189693a87d4156f271fbbbb8c3b7ef8c5b49a1625c'
+      def install
+        bin.install "vmatch"
+      end
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/anttiharju/vmatch/releases/download/v0.1.8/vmatch-darwin-arm64.tar.gz"
+      sha256 ''
+
+      def install
+        bin.install "vmatch"
+      end
+    end
   end
 
-  def install
-    ENV['GOPATH'] = buildpath
+  on_linux do
+    if Hardware::CPU.intel?
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/anttiharju/vmatch/releases/download/v0.1.8/vmatch-linux-amd64.tar.gz"
+        sha256 ''
 
-    bin_path = buildpath / 'src/github.com/anttiharju/vmatch'
-    bin_path.install Dir['*']
-    cd bin_path do
-      system 'go', 'build', '-ldflags', '-s -w -buildid=brew-v0.1.7', '-trimpath'
-      bin.install 'vmatch'
+        def install
+          bin.install "vmatch"
+        end
+      end
+    end
+    if Hardware::CPU.arm?
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/anttiharju/vmatch/releases/download/v0.1.8/vmatch-linux-arm64.tar.gz"
+        sha256 ''
+
+        def install
+          bin.install "vmatch"
+        end
+      end
     end
   end
 
   test do
-    system 'vmatch', 'doctor'
+    system "#{bin}/vmatch doctor"
   end
 end
